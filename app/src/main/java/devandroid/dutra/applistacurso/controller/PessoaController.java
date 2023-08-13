7426ff36db5d3cec280aa49c9f587195ca14d933
virusDutra <94208590+virusDutra@users.noreply.github.com>
2023-08-13 17:44:51 -0300

    MVC - Controller

--- app/src/main/java/devandroid/dutra/applistacurso/controller/PessoaController.java
+++ app/src/main/java/devandroid/dutra/applistacurso/controller/PessoaController.java
@@ -1,4 +1,25 @@
 package devandroid.dutra.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.dutra.applistacurso.model.Pessoa;
+
 public class PessoaController {
+
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC Controller", "Controller iniciada...");
+
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC Controller", "Salvo: "+pessoa.toString());
+
+    }
 }
